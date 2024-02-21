86d89d0a5269064a0b9a28daa54de8ec12d72fd0
Leonardo V de Knegt <79206554+lvdeknegt@users.noreply.github.com>
2024-02-21 11:54:26 +0100

    Update DLM functions - DECIDE deliverable.R
    Correction to the Kalman smoother in line 618.

--- DLM functions - DECIDE deliverable.R
+++ DLM functions - DECIDE deliverable.R
@@ -615,7 +615,7 @@ runSmoother <- function(res) {
   for(i in ((n-1):1))   {
     
     # Get Gt
-    Gt <- res$Gt.list[[i]]
+    Gt <- res$Gt.list[[i+1]]
     
     #print(i)
     res$R[[i+1]] <- as.matrix(res$R[[i+1]])
